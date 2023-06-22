71a4427d6e787f3091e2e9143564a87b9c479e51
3dendroid <42907989+3dendroid@users.noreply.github.com>
2023-06-22 21:03:36 +0700

    add app.py and app2.py
    first version of videogrep + ffmpeg

---
+++ app2.py
@@ -0,0 +1,7 @@
+# working script, not a final version
+
+from videogrep import videogrep
+
+video = r'C:\Users\GIGACHAD\PycharmProjects\Video_GREP\two.mp4'  # don't undestand how it works but it wokrs, see command.txt
+
+videogrep(video, 'device')
---
+++ vtt_to_srt.py
@@ -0,0 +1,6 @@
+# program for convert vtt to srt
+
+from vtt_to_srt.vtt_to_srt import ConvertFile  # don't understand why it not works
+
+convert_file = ConvertFile(r"C:\Users\GIGACHAD\PycharmProjects\Video_GREP\1.vtt", "utf-8")
+convert_file.convert()
