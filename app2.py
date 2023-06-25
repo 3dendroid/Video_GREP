cce59e9d98439eb72546e933d17711ebf7ac13a4
3dendroid <42907989+3dendroid@users.noreply.github.com>
2023-06-25 21:18:56 +0700

    rewrite scripts
    manual rewrite scripts

--- app2.py
+++ app2.py
@@ -2,6 +2,8 @@
 
 from videogrep import videogrep
 
-video = r'C:\Users\GIGACHAD\PycharmProjects\Video_GREP\two.mp4'  # don't undestand how it works but it wokrs, see command.txt
+video = r'C:\Users\GIGACHAD\Desktop\videogrep2\source_videos\apple.mp4'  # don't undestand how it works but it wokrs, see command.txt
 
-videogrep(video, 'device')
+word = input("Enter word to search:")  # add input for usability
+
+videogrep(video, word)
--- info_yt_dlp.py
+++ info_yt_dlp.py
@@ -5,10 +5,10 @@ import yt_dlp
 
 URL = 'https://www.youtube.com/watch?v=1psQmdieFsU&ab_channel=DorothyGale'
 
-# ℹ️ See help(yt_dlp.YoutubeDL) for a list of available options and public functions
+# See help(yt_dlp.YoutubeDL) for a list of available options and public functions
 ydl_opts = {}
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     info = ydl.extract_info(URL, download=False)
 
-    # ℹ️ ydl.sanitize_info makes the info json-serializable
+    # ydl.sanitize_info makes the info json-serializable
     print(json.dumps(ydl.sanitize_info(info)))
